3c11af06410b46b6108a03875af0f118cbaf2cac
Stephen Karger <skarger@salsify.com>
2017-07-28 15:18:31 -0400

    adjust relationships

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api/organizations',
+  namespace: 'api',
   host: 'http://localhost:21012',
   pathForType: function(type) {
     return Ember.String.underscore(type).pluralize();
--- app/models/organization.js
+++ app/models/organization.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  flowTemplates: DS.hasMany('flowTemplates'),
-
+  flowTemplates: DS.hasMany('flow-template'),
+  name: DS.attr('string'),
 });
