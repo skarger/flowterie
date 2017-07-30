52ce2668a2949c7a71748e9631e39705d1777527
Stephen Karger <skarger@salsify.com>
2017-07-30 11:04:11 -0400

    Change to dandelion api path

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,10 +3,10 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   namespace: 'api',
-  host: 'http://localhost:21012',
+  host: 'http://localhost:5000',
   pathForType: function(type) {
     var org = this.get('store').peekRecord('organization', 'a1');
-    var prefix = 'organizations/' + org.get('id') + '/';
+    var prefix = 'orgs/' + org.get('id') + '/';
     return prefix + Ember.String.underscore(type).pluralize();
   },
 });
--- mirage/config.js
+++ mirage/config.js
@@ -8,12 +8,12 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  this.urlPrefix = 'http://localhost:21012';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = 'http://localhost:5000';    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
   //
   this.organization_id = 'a1';
-  this.prefix = 'organizations/' + this.organization_id;
+  this.prefix = 'orgs/' + this.organization_id;
 
   /*
     Shorthand cheatsheet:
