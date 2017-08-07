1a0103f1f1a1ada9250395918281342c7056a68d
Stephen Karger <skarger@salsify.com>
2017-08-07 16:02:46 -0400

    Working test for login component - service integration

--- app/components/login-input.js
+++ app/components/login-input.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
 
   organiztionIdInput: "",
   apiTokenInput: "",
+  redirect: function() {},
 
   actions: {
     submitLogin() {
--- tests/integration/components/login-input-test.js
+++ tests/integration/components/login-input-test.js
@@ -1,5 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+import { fillIn, click } from 'ember-native-dom-helpers';
 
 moduleForComponent('login-input', 'Integration | Component | login input', {
   integration: true
@@ -15,4 +17,21 @@ test('it renders', function(assert) {
   assert.equal(this.$('input#organization-id').length, 1);
   assert.equal(this.$('input#api-token').length, 1);
   assert.equal(this.$().text().trim(), 'Login');
+});
+
+test('it sets credentials', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.inject.service('identity');
+  this.render(hbs`{{login-input}}`);
+
+  Ember.run(() => {
+    fillIn('input#organization-id', 'abc');
+    fillIn('input#api-token', 'def');
+    click('button#login');
+  });
+
+  assert.equal(this.get('identity.present'), true);
 });
