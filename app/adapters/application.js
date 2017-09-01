40b2922011a5068a130859de398821f0e0d964a8
Stephen Karger <skarger@salsify.com>
2017-09-01 17:22:34 -0400

    Change API port number for dev since we need to proxy it anyway

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,12 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   identity: Ember.inject.service(),
   namespace: 'api',
-  host: 'http://localhost:5000',
+  host: 'http://localhost:4200',
+  headers: Ember.computed('identity.apiToken', function() {
+    return {
+      'Authorization': 'Bearer ' + this.get('identity.apiToken')
+    };
+  }),
   pathForType: function(type) {
     var prefix = 'orgs/' + this.get('identity').get('organizationId') + '/content_flow/';
     return prefix + Ember.String.underscore(type).pluralize();
--- mirage/config.js
+++ mirage/config.js
@@ -8,7 +8,7 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  this.urlPrefix = 'http://localhost:5000';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = 'http://localhost:4200';    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
   //
